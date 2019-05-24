212b379fa91d89d7af73257a524fc742f675a278
thajedster <jedlawrencecheng@gmail.com>
2019-05-23 20:37:12 -0400

    made a hello world on root endpoint

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/api-routes")(app);
+
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
