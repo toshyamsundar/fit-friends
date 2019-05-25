118ca25f13577f20caf44e7ad091c4f1095b0db4
thajedster <jedlawrencecheng@gmail.com>
2019-05-25 11:34:17 -0400

    added html routes and user api route and added path to package

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
