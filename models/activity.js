bd1f0be7f365f3079b527153b1629c137931f36b
toshyamsundar <toshyamsundar@yahoo.com>
2019-05-28 20:02:59 -0400

    added metric column back to the activities model

--- models/activity.js
+++ models/activity.js
@@ -3,6 +3,10 @@ module.exports = function(sequelize, DataTypes) {
     name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    metric: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   });
 
