6323397d8207a0067cab87f1327d058d9f5f4227
toshyamsundar <toshyamsundar@yahoo.com>
2019-05-30 15:51:58 -0400

    Added user id to session storage and moved app.js below the jquery cdn

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,31 @@
-$(document).on("click", "#btn-create", event => {
-  let userProfile = {};
+$(document).ready(() => {
+  $(document).on("click", "#btn-create", event => {
+    let userProfile = {};
+    let userGoal = [];
+    let user;
 
-  let userGoal = [];
+    userProfile.username = $("#username").val();
+    userProfile.name = $("#fullname").val();
+    userProfile.age = $("#user-age").val();
+    userProfile.height = $("#user-height").val();
+    userProfile.weight = $("#user-weight").val();
 
-  userProfile.username = $("#username").val();
-  userProfile.name = $("#fullname").val();
-  userProfile.age = $("#user-age").val();
-  userProfile.height = $("#user-height").val();
-  userProfile.weight = $("#user-weight").val();
+    $.post("/api/user", userProfile).then(userResponse => {
+      $("#goal-section")
+        .children(".form-group")
+        .each(function() {
+          let goalName = $(this).attr("data-goal");
+          let goalMetric = $(this)
+            .children(".form-control")
+            .val();
+          user = userResponse.id;
+          userGoal.push({ name: goalName, metric: goalMetric, UserId: user });
+        });
 
-  $.post("/api/user", userProfile).then(userResponse => {
-    $("#goal-section")
-      .children(".form-group")
-      .each(function() {
-        let goalName = $(this).attr("data-goal");
-        let goalMetric = $(this)
-          .children(".form-control")
-          .val();
-        let user = userResponse.id;
-        userGoal.push({ name: goalName, metric: goalMetric, UserId: user });
+      $.post("/api/goals", { userGoal }).then(goalResponse => {
+        console.log(goalResponse);
+        sessionStorage.setItem("userId", user);
       });
-
-    $.post("/api/goals", { userGoal }).then(goalResponse => {
-      console.log(goalResponse);
     });
   });
 });
