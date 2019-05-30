56f92bb0490bcee993f830f1fdf99c2e4a062322
toshyamsundar <toshyamsundar@yahoo.com>
2019-05-29 20:33:40 -0400

    Updated endpoints in the html file to render them from node Updated goals endpoint to activities

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,15 @@
 var path = require("path");
 
-module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
-    app.get("/profile", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/profile.html"));
-    });
+  app.get("/profile", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/profile.html"));
+  });
 
-    app.get("/goals", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/goals.html"));
-    });
-}
+  app.get("/activities", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/goals.html"));
+  });
+};
